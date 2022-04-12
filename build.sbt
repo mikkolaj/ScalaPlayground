7bc9b1aa0a798c2808d5c0ae4b92bbb938b572ca
mikkolaj <bulmikolaj@interia.pl>
2022-04-12 21:23:45 +0200

    Requests with AkkaHttp.

--- build.sbt
+++ build.sbt
@@ -1,12 +1,22 @@
 ThisBuild / version := "0.1.0-SNAPSHOT"
 
-ThisBuild / scalaVersion := "2.11.12"
+ThisBuild / scalaVersion := "2.13.8"
 
 lazy val root = (project in file("."))
   .settings(
     name := "ScalaSBT"
   )
 
-libraryDependencies += "org.scalamock" %% "scalamock" % "4.4.0" % Test
-libraryDependencies += "org.scalatest" %% "scalatest" % "3.2.11" % Test
-libraryDependencies += "org.mockito" %% "mockito-scala" % "1.17.5" % Test
+val akkaVersion = "2.6.19"
+val akkaHttpVersion = "10.2.9"
+
+libraryDependencies ++= Seq(
+  // akka streams
+  "com.typesafe.akka" %% "akka-stream" % akkaVersion,
+  // akka http
+  "com.typesafe.akka" %% "akka-http" % akkaHttpVersion,
+  "com.typesafe.akka" %% "akka-http-spray-json" % akkaHttpVersion,
+  "org.scalamock" %% "scalamock" % "4.4.0" % Test,
+  "org.scalatest" %% "scalatest" % "3.2.11" % Test,
+  "org.mockito" %% "mockito-scala" % "1.17.5" % Test,
+)
---
+++ src/main/scala/akkahttp/RequestSender.scala
@@ -0,0 +1,31 @@
+package akkahttp
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.{HttpMethods, HttpRequest}
+
+import scala.concurrent.Future
+import scala.concurrent.duration._
+
+// Based on: https://blog.rockthejvm.com/a-5-minute-akka-http-client/
+
+object RequestSender extends App {
+  implicit val system: ActorSystem = ActorSystem() // akkaActors
+
+  import system.dispatcher // "thread pool", used to serve http requests
+
+  // akkaHttp uses akkaStreams
+
+  val request = HttpRequest(
+    method = HttpMethods.GET,
+    uri = "https://pokeapi.co/api/v2/pokemon/ditto/"
+  )
+
+  def sendRequest(httpRequest: HttpRequest): Future[String] = {
+    val responseFuture = Http().singleRequest(httpRequest)
+    val entityFuture = responseFuture.flatMap(_.entity.toStrict(2.seconds))
+    entityFuture.map(_.data.utf8String)
+  }
+
+  sendRequest(request).foreach(println)
+}
