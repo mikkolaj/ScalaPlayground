74f783fc17923e085cec392711c1af9466aa5da5
mikkolaj <bulmikolaj@interia.pl>
2022-04-20 19:03:39 +0200

    tweet example

--- src/main/scala/akka_streams/datastructures/Tweet.scala
+++ src/main/scala/akka_streams/datastructures/Tweet.scala
@@ -1,7 +1,5 @@
 package akka_streams.datastructures
 
-import akka_streams.datastructures.Author
-
 final case class Tweet(author: Author, timestamp: Long, body: String) {
   def hashtags: Set[Hashtag] =
     body
